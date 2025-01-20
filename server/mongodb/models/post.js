618ada06c25203bfe9b0019188aa5adff0f55036
Mehul Anand <89949898+mehul-anand@users.noreply.github.com>
2025-01-21 02:45:07 +0530

    post model

---
+++ server/mongodb/models/post.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+
+const ImgPost = new mongoose.Schema({
+    postId:{type:String,required:true,unique:true},
+    artist:{type:String,required:true},
+    username:{type:String,required:true},
+    title:{type:String,required:true},
+    userId : {type:String,required:true},
+    userMail:{type:String,required:true},
+    prompt:{type:String,required:true},
+    photo:{type:String,required:true},
+})
+
+const PostSchema = mongoose.model("ImgPost",ImgPost)
+
+export default PostSchema
