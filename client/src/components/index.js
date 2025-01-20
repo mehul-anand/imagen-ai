cb3d219662fc8f67af8eb9916b2d4d4187c33e1a
Mehul Anand <89949898+mehul-anand@users.noreply.github.com>
2025-01-21 02:40:08 +0530

    components

---
+++ client/src/components/index.js
@@ -0,0 +1,7 @@
+import Navbar from './Navbar';
+import Card from './Card';
+import FormField from './FormField';
+import Loader from './Loader';
+import CardGrid from './CardGrid';
+
+export default {Navbar,Card,FormField,Loader,CardGrid};
