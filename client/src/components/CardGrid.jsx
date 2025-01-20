3414003a9af4cc8585edc31bdacb6e75e29f3cb9
Mehul Anand <89949898+mehul-anand@users.noreply.github.com>
2025-01-21 02:39:28 +0530

    card grid

---
+++ client/src/components/CardGrid.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Card from "./Card";
+
+const CardGrid = ({ data, errorMessage }) => {
+  console.log("CardGrid data:", data);
+  if (data && data?.length > 0) {
+    return data.map((post) => <Card key={post._id} {...post} />);
+  }
+
+  return (
+    <h2 className="mt-5 font-bold text-[#6469ff] text-xl uppercase">{errorMessage}</h2>
+  );
+};
+
+export default CardGrid;
