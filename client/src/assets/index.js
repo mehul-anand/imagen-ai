aea3095fb7d6145d56f845fac8a70b7d8e24c672
Mehul Anand <89949898+mehul-anand@users.noreply.github.com>
2025-01-21 02:38:50 +0530

    assets folder

---
+++ client/src/assets/index.js
@@ -0,0 +1,12 @@
+import download from './download.png';
+import logo from './logo.svg';
+import preview from './preview.png';
+import mail from "./mail.png"
+
+
+export {
+  download,
+  logo,
+  preview,
+  mail
+};
