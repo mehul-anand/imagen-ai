6123bbb38c0604818c9e36150c1a461911360091
Mehul Anand <89949898+mehul-anand@users.noreply.github.com>
2025-01-22 08:27:48 +0530

    exports

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -1,5 +1,6 @@
+import Community from "./Community";
 import Home from "./Home";
 import CreatePost from "./CreatePost";
 import Layout from "./Layout";
 
-export default {Home, CreatePost,Layout}
+export default {Home , Community, CreatePost,Layout}
