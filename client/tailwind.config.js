a1b2833067004378a10a0f19abf7083dc25242ba
Mehul Anand <89949898+mehul-anand@users.noreply.github.com>
2025-01-22 08:42:48 +0530

    Revert "config"
    This reverts commit 3e655b81fb87d3e83c78a8c4349766f9ad7ba7dc.

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,13 +1,9 @@
 /** @type {import('tailwindcss').Config} */
-import defaultTheme from "tailwindcss/defaultTheme";
-import colors from "tailwindcss/colors";
-import flattenColorPalette from "tailwindcss/lib/util/flattenColorPalette";
 export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
-  darkMode: "class",
   theme: {
     extend: {
       screens: {
@@ -35,16 +31,5 @@ export default {
   }
     },
   },
-  plugins: [addVariablesForColors],
-}
-
-function addVariablesForColors({ addBase, theme }) {
-  let allColors = flattenColorPalette(theme("colors"));
-  let newVars = Object.fromEntries(
-    Object.entries(allColors).map(([key, val]) => [`--${key}`, val])
-  );
-
-  addBase({
-    ":root": newVars,
-  });
+  plugins: [],
 }
