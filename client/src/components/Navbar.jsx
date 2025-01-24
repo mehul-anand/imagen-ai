6a57b9b174d69ec23360eb882fe7703100d3aaf0
Mehul Anand <89949898+mehul-anand@users.noreply.github.com>
2025-01-24 16:59:53 +0530

    useMatch implementation

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useMatch } from "react-router-dom";
 import logo from "../assets/logo.svg";
 import {
   SignedIn,
@@ -10,7 +10,8 @@ import {
 } from "@clerk/clerk-react";
 
 function Navbar() {
-  
+  const createMatch = useMatch("/create-post");
+  // const communityMatch = useMatch("/commmunity")
   return (
     <header className="w-full flex justify-between items-center bg-black sm:px-8 px-4 py-4 border-b border-b-[#6469ff] mb-10">
       <Link to="/">
@@ -18,36 +19,49 @@ function Navbar() {
       </Link>
       <div className="flex justify-between gap-3">
         <SignedIn>
-           <Link
-             to="/create-post"
-           >
-             <button className="p-[3px] relative">
-               <div className="absolute inset-0 bg-gradient-to-r from-indigo-500 to-purple-500 rounded-lg" />
-               <div className="px-8 py-2  bg-black rounded-[6px]  relative group transition duration-200 text-white hover:bg-transparent">
-                 Create Art
-               </div>
-             </button>
-           </Link>
-          <UserButton/>
+          {createMatch ? (
+            <>
+              <Link to="/community">
+                <button className="p-[3px] relative">
+                  <div className="absolute inset-0 bg-gradient-to-r from-indigo-500 to-purple-500 rounded-lg" />
+                  <div className="px-8 py-2  bg-black rounded-[6px]  relative group transition duration-200 text-white hover:bg-transparent">
+                    Community
+                  </div>
+                </button>
+              </Link>
+            </>
+          ) : (
+            <>
+              <Link to="/create-post">
+                <button className="p-[3px] relative">
+                  <div className="absolute inset-0 bg-gradient-to-r from-indigo-500 to-purple-500 rounded-lg" />
+                  <div className="px-8 py-2  bg-black rounded-[6px]  relative group transition duration-200 text-white hover:bg-transparent">
+                    Create Art
+                  </div>
+                </button>
+              </Link>
+            </>
+          )}
+          <UserButton />
         </SignedIn>
         <SignedOut>
           <SignInButton>
-          <button className="p-[3px] relative">
-               <div className="absolute inset-0 bg-gradient-to-r from-indigo-500 to-purple-500 rounded-lg" />
-               <div className="sm:px-8 sm:py-2 px-3 py-2 relative group transition duration-200 text-white">
-                 Sign In
-               </div>
-             </button>
+            <button className="p-[3px] relative">
+              <div className="absolute inset-0 bg-gradient-to-r from-indigo-500 to-purple-500 rounded-lg" />
+              <div className="sm:px-8 sm:py-2 px-3 py-2 relative group transition duration-200 text-white">
+                Sign In
+              </div>
+            </button>
           </SignInButton>
-          
-          <SignUpButton >
-          <button className="p-[3px] relative">
-               <div className="absolute inset-0 bg-gradient-to-r from-indigo-500 to-purple-500 rounded-lg" />
-               <div className="sm:px-8 sm:py-2 px-3 py-2 bg-black rounded-[6px]  relative group transition duration-200 text-white hover:bg-transparent">
-                 Sign Up
-               </div>
-             </button>
-            </SignUpButton>
+
+          <SignUpButton>
+            <button className="p-[3px] relative">
+              <div className="absolute inset-0 bg-gradient-to-r from-indigo-500 to-purple-500 rounded-lg" />
+              <div className="sm:px-8 sm:py-2 px-3 py-2 bg-black rounded-[6px]  relative group transition duration-200 text-white hover:bg-transparent">
+                Sign Up
+              </div>
+            </button>
+          </SignUpButton>
         </SignedOut>
       </div>
     </header>
